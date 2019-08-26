8116d9fcf08bc5703dbac1287fcd96073e6bfa89
Felipe Plets <felipe.plets@menvia.com>
2019-08-25 21:47:20 -0400

    Add global object to store loaded extensions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ class FarolExtension {
   constructor(public config: any) {}
   register(trigger: string, handler: (...args: any[]) => void) {
     eventEmitter.on(`${this.config.id}_${trigger}`, handler);
+
+    // Add to the global open extension functions
+    if (!global.openExtension[this.config.id]) {
+      global.openExtension[this.config.id] = {};
+    }
+    global.openExtension[this.config.id][trigger] = handler;
   }
 }
 export { eventEmitter, FarolExtension };
